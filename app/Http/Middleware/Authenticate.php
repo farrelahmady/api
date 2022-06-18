b3e38c03ed40d0e0dfc4c4518a63a5dca700cdbf
Farrel Ahmad Yudithia <farrelfay.ce@gmail.com>
2022-06-18 18:17:06 +0700

    AuthMiddleware response

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Helpers\ResponseFormatter;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
@@ -15,7 +16,7 @@ class Authenticate extends Middleware
   protected function redirectTo($request)
   {
     if (!$request->expectsJson()) {
-      return route('login');
+      abort(ResponseFormatter::error(['error' => 'Unauthorized'], 'Unauthorized', 401));
     }
   }
 }
