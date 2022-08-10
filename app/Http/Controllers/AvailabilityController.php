d051e1695566596c280c6fdeb54f79a753b93679
Farrel Ahmad Yudithia <farrelfay.ce@gmail.com>
2022-08-10 10:34:55 +0700

    addd notfound condition

--- app/Http/Controllers/AvailabilityController.php
+++ app/Http/Controllers/AvailabilityController.php
@@ -10,70 +10,74 @@ use Illuminate\Http\Request;
 
 class AvailabilityController extends Controller
 {
-    public function index(Request $req)
-    {
-        try {
-            $tailor = $req->input('tailor');
+  public function index(Request $req)
+  {
+    try {
+      $tailor = $req->input('tailor');
 
-            $data = collect();
-            if (count($req->all()) <= 0) {
-                if (!auth('sanctum')->check()) {
-                    return ResponseFormatter::error(message: 'Unauthorized', code: 401);
-                }
-                if (!in_array($req->user('sanctum')->uuid, UserAdmin::pluck('uuid')->toArray())) {
-                    return ResponseFormatter::error(["message" => "Kamu tidak memiliki akses"], 'Forbidden', 403);
-                }
-                $availability = Availability::all();
-            } else {
-                $availability = Availability::where('user_tailor_id', $tailor)->get();
-            }
-
-
-            // return ResponseFormatter::success($data, 'Data berhasil didapatkan');
-            // $availability->keys()->each(function ($item) use ($data, $availability) {
-            //   $time = collect();
-            //   $availability[$item]->each(function ($item) use ($time) {
-            //     $time->push($item->time);
-            //   });
-            //   $data->push([
-            //     'date' => $item,
-            //     'time' => $time
-            //   ]);
-            // });
-
-            $availability = $availability->groupBy('user_tailor_id');
-
-            $availability->keys()->each(function ($key) use ($data, $availability) {
-                // return $availability;
-                $schedule = collect();
-                $date = $availability[$key]->groupBy("date");
-                $date->keys()->each(function ($item) use ($key, $date, $schedule) {
-                    $time = collect();
-                    $date[$item]->each(function ($item) use ($time) {
-                        $time->push(["time" => $item->time, "booked" => false]);
-                    });
-                    $schedule->push(
-                        [
-                            "date" => $item,
-                            "time" => $time,
-                        ]
-                    );
-                });
-
-                $data->push([
-                    'user_tailor_id' => $key,
-                    'schedule' => $schedule,
-                ]);
-            });
-
-            if ($tailor) {
-                $data = $data->where('user_tailor_id', $tailor)->first()["schedule"];
-            }
-
-
-            return ResponseFormatter::success($data, $data->count() . ' Data berhasil didapatkan');
-        } catch (\Exception $e) {
-            ResponseFormatter::error(message: $e->getMessage(), code: 500);
+      $data = collect();
+      if (count($req->all()) <= 0) {
+        if (!auth('sanctum')->check()) {
+          return ResponseFormatter::error(message: 'Unauthorized', code: 401);
         }
+        if (!in_array($req->user('sanctum')->uuid, UserAdmin::pluck('uuid')->toArray())) {
+          return ResponseFormatter::error(["message" => "Kamu tidak memiliki akses"], 'Forbidden', 403);
+        }
+        $availability = Availability::all();
+      } else {
+        $availability = Availability::where('user_tailor_id', $tailor)->get();
+      }
+
+      if ($availability->count() <= 0) {
+        return ResponseFormatter::error(["message" => "Data tidak ditemukan"], 'Not Found', 404);
+      }
+
+
+      // return ResponseFormatter::success($data, 'Data berhasil didapatkan');
+      // $availability->keys()->each(function ($item) use ($data, $availability) {
+      //   $time = collect();
+      //   $availability[$item]->each(function ($item) use ($time) {
+      //     $time->push($item->time);
+      //   });
+      //   $data->push([
+      //     'date' => $item,
+      //     'time' => $time
+      //   ]);
+      // });
+
+      $availability = $availability->groupBy('user_tailor_id');
+
+      $availability->keys()->each(function ($key) use ($data, $availability) {
+        // return $availability;
+        $schedule = collect();
+        $date = $availability[$key]->groupBy("date");
+        $date->keys()->each(function ($item) use ($key, $date, $schedule) {
+          $time = collect();
+          $date[$item]->each(function ($item) use ($time) {
+            $time->push(["time" => $item->time, "booked" => false]);
+          });
+          $schedule->push(
+            [
+              "date" => $item,
+              "time" => $time,
+            ]
+          );
+        });
+
+        $data->push([
+          'user_tailor_id' => $key,
+          'schedule' => $schedule,
+        ]);
+      });
+
+      if ($tailor) {
+        $data = $data->where('user_tailor_id', $tailor)->first()["schedule"];
+      }
+
+
+      return ResponseFormatter::success($data, $data->count() . ' Data berhasil didapatkan');
+    } catch (\Exception $e) {
+      ResponseFormatter::error(message: $e->getMessage(), code: 500);
     }
+  }
 }
