c35b8906e89c41770646e87472297b84a0d4397f
Farrel Ahmad Yudithia <farrelfay.ce@gmail.com>
2022-08-15 15:02:43 +0700

    test appointmentController

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -13,7 +13,7 @@ class AppointmentController extends Controller
 {
     public function store(Request $req)
     {
-        return ResponseFormatter::success($req->all(), 'Data berhasil ditambahkan');
+        // return ResponseFormatter::success($req->all(), 'Data berhasil ditambahkan');
         try {
             Carbon::setLocale('id');
 
